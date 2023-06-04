b7425c99200e2b49faebce9ea8d01d4c0ad13262
roma_marshall <roma_marshall@outlook.com>
2023-06-04 21:49:27 +0200

    refactoring mobile view

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
     <Circle class="hidden md:block absolute top-0 left-0 -translate-x-1/4 -translate-y-1/4 opacity-40" />
     <div class="hidden md:block bg-gray-900 inset-y-0 right-0 w-2/5 absolute"></div>
     <div class="mx-auto max-w-7xl px-4 pt-8 relative">
+      <!-- Desktop Navbar -->
       <nav class="flex items-center justify-between">
-        <div class="flex items-center gap-32">
-          <a href="#" class="text-2xl font-bold">Razor</a>
+        <a href="#" class="text-2xl font-bold">Razor</a>
+        <div class="hidden md:flex items-center gap-32">
           <ul class="flex items-center gap-12 font-medium text-gray-500">
             <li>
               <a href="#" class="group hover:text-white transition duration-300">
@@ -39,13 +40,54 @@
             </li>
           </ul>
         </div>
-        <div class="flex items-center gap-4 font-medium text-gray-500">
+        <button @click="showNav()" type="button" class="block md:hidden inline-flex items-center p-2 ml-3 text-sm text-gray-500">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"></path>
+          </svg>
+        </button>
+        <div class="hidden md:flex items-center gap-4 font-medium text-gray-500">
           <a href="#" class="hover:text-white">Sign In</a>
           <a href="#" class="relative hover:text-white">
             <span>Request Demo</span>
             <img src="/img/decoration.svg" alt="" class="w-32 absolute left-0" />
           </a>
         </div>
+      </nav>
+      <!-- Mobile Navbar -->
+      <nav :class="{hidden: isActive}" class="md:hidden bg-gray-900 rounded items-center">
+        <ul class="font-medium text-gray-500 px-4 py-3">
+          <li>
+            <a href="#" class="group hover:text-white transition duration-300">
+              Product
+              <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-teal-500"></span>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="group hover:text-white transition duration-300">
+              Resource
+              <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-teal-500"></span>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="group hover:text-white transition duration-300">
+              Support
+              <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-teal-500"></span>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="group hover:text-white transition duration-300">
+              Pricing
+              <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-teal-500"></span>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="group hover:text-white transition duration-300">
+              Blog
+              <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-teal-500"></span>
+            </a>
+          </li>
+        </ul>
       </nav>
       <div class="grid grid-cols-1 md:grid-cols-2 pt-6">
         <div class="max-w-xl pr-24">
@@ -60,11 +102,11 @@
               &#8212; We'll send you a download link
             </h1>
             <img src="/img/decoration.svg" alt="" class="-mt-2 w-96 absolute left-0" />
-            <div class="flex items-center mt-10 shadow-glow focus-within:shadow-teal-500/40 transition-shadow ease-out rounded-xl">
+            <div class="grid lg:flex items-center mt-10 shadow-glow focus-within:shadow-teal-500/40 transition-shadow ease-out rounded-xl">
               <input type="text"
                      placeholder="sales@xstudio.dev"
-                     class="flex-1 rounded-l-xl border-2 border-r-0 border-gray-700 bg-gray-900 leading-6 focus:outline-none px-8 py-4">
-              <button class="px-8 py-4 rounded-r-xl border-2 border-l-0 border-gray-700 bg-teal-500/90 hover:bg-teal-500 transition-colors duration-300 text-gray-900 text-lg leading-6 font-bold">Get Link</button>
+                     class="flex-1 lg:rounded-l-xl border-2 lg:border-r-0 border-gray-700 bg-gray-900 leading-6 focus:outline-none px-8 py-4">
+              <button class="px-8 py-4 lg:rounded-r-xl border-2 lg:border-l-0 border-gray-700 bg-teal-500/90 hover:bg-teal-500 transition-colors duration-300 text-gray-900 text-lg leading-6 font-bold">Get Link</button>
             </div>
             <p class="pt-8 font-normal tracking-wide text-gray-600">
               By signing up, I agree to the Razor
@@ -86,7 +128,7 @@
         <div class="pt-10 hidden md:block">
           <div class="flex justify-center gap-8">
             <img src="/img/screen-1.png" alt="" class="h-full w-64 animate-hover" />
-            <img src="/img/screen-2.png" alt="" class="h-full w-64 pt-20 animate-hover" />
+            <img src="/img/screen-2.png" alt="" class="hidden lg:block h-full w-64 pt-20 animate-hover" />
           </div>
         </div>
       </div>
@@ -95,10 +137,17 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import ArrowRight from './icons/arrow-right.svg'
 import AppleIcon from './icons/apple.svg'
 import PlayStoreIcon from './icons/play-store.svg'
 import Circle from '../public/img/circle.svg'
+
+const isActive = ref(true)
+function showNav() {
+  this.isActive = !this.isActive;
+}
+
 </script>
 
 <style scoped>
